3de4ccba2c6736055b7188c3b3bc358aaead6bc9
Lim Haksu <neroren2@gmail.com>
2021-02-21 06:21:58 +0900

    chore: 통계페이지 제작중 안내문구

--- src/page/StatisticsPage.tsx
+++ src/page/StatisticsPage.tsx
@@ -1,5 +1,9 @@
 interface Props {}
 
 export const StatisticsPage = (props: Props) => {
-    return <div>통계 페이지</div>;
+    return (
+        <div style={{ height: "calc(100vh - 128px)", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            통계 페이지 제작중 ...
+        </div>
+    );
 };
