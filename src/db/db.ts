0c3cef9b3e977f50a56d6a5ef5c66b35e5e1beb1
Lim Haksu <neroren2@gmail.com>
2021-02-23 17:54:44 +0900

    fix: sqlite 외래키 제약조건 기본이 false여서 설정을 별도로 함

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,6 +4,7 @@ const { remote } = window.require("electron");
 const sqlite3 = window.require("sqlite3").verbose();
 const dbFile = path.join(remote.app.getAppPath(), path.sep + "database.db");
 const db = new sqlite3.Database(dbFile);
+db.get("PRAGMA foreign_keys = ON");
 export default db;
 
 export interface Category {
